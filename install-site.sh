c09beb54a19a7a55f0d08484d0fb448aec142988
Marcus Bergo <marcus@bergo.one>
2023-11-03 02:37:00 -0300

    refactoring all

--- install-site.sh
+++ install-site.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
 
-# Define the source directory for your Django project
-# Replace this with the actual path to your Django project
-DJANGO_PROJECT_PATH="/path/to/your/django/project"
-DJANGO_PROJECT_NAME="myproject" # Replace with your actual Django project name
+# Define the name of your Django project
+PROJECT_NAME="<your_project_name>"
+
+# Define the root directory of your cloned repository
+REPO_ROOT_DIR="<repository_root_directory>"
 
 # Define the Bitnami specific paths
-BITNAMI_PYTHON="/opt/bitnami/python/bin/python"
-BITNAMI_MANAGE_PY="/opt/bitnami/apps/django/django_projects/$DJANGO_PROJECT_NAME/manage.py"
-BITNAMI_PROJECTS_PATH="/opt/bitnami/apps/django/django_projects"
-BITNAMI_STATIC_PATH="/opt/bitnami/apps/django/django_projects/$DJANGO_PROJECT_NAME/static"
-BITNAMI_MEDIA_PATH="/opt/bitnami/apps/django/django_projects/$DJANGO_PROJECT_NAME/media"
+BITNAMI_BASE_DIR="/opt/bitnami"
+BITNAMI_PYTHON="$BITNAMI_BASE_DIR/python/bin/python"
+BITNAMI_PROJECTS_DIR="$BITNAMI_BASE_DIR/apps/django/django_projects/$PROJECT_NAME"
+BITNAMI_STATIC_DIR="$BITNAMI_PROJECTS_DIR/static"
+BITNAMI_MEDIA_DIR="$BITNAMI_PROJECTS_DIR/media"
+BITNAMI_MANAGE_PY="$BITNAMI_PROJECTS_DIR/manage.py"
 
-# Copy project to Bitnami projects directory
-echo "Copying Django project to Bitnami projects directory..."
-cp -r "$DJANGO_PROJECT_PATH" "$BITNAMI_PROJECTS_PATH/$DJANGO_PROJECT_NAME"
+# Prompt for database credentials
+read -p "Enter your MariaDB database name: " db_name
+read -p "Enter your MariaDB database user: " db_user
+read -sp "Enter your MariaDB database password: " db_password
+echo
+read -p "Enter your MariaDB host (default 'localhost'): " db_host
+db_host=${db_host:-localhost}
+read -p "Enter your MariaDB port (default '3306'): " db_port
+db_port=${db_port:-3306}
 
-# Navigate to the Bitnami projects directory
-cd "$BITNAMI_PROJECTS_PATH/$DJANGO_PROJECT_NAME" || exit
+# Create the necessary directories
+echo "Creating directories..."
+mkdir -p "$BITNAMI_PROJECTS_DIR"
+mkdir -p "$BITNAMI_STATIC_DIR"
+mkdir -p "$BITNAMI_MEDIA_DIR"
+
+# Copy files from the repository root to the Bitnami project directory
+echo "Copying files..."
+cp -a "$REPO_ROOT_DIR/." "$BITNAMI_PROJECTS_DIR/"
+
+# Navigate to the Bitnami project directory
+cd "$BITNAMI_PROJECTS_DIR" || exit
+
+# Replace placeholders in settings.py with actual database credentials
+echo "Configuring settings.py with database credentials..."
+sed -i "s/'NAME': 'your-database-name'/'NAME': '$db_name'/g" "$BITNAMI_PROJECTS_DIR/EvenSight/settings.py"
+sed -i "s/'USER': 'your-database-user'/'USER': '$db_user'/g" "$BITNAMI_PROJECTS_DIR/EvenSight/settings.py"
+sed -i "s/'PASSWORD': 'your-database-password'/'PASSWORD': '$db_password'/g" "$BITNAMI_PROJECTS_DIR/EvenSight/settings.py"
+sed -i "s/'HOST': 'localhost'/'HOST': '$db_host'/g" "$BITNAMI_PROJECTS_DIR/EvenSight/settings.py"
+sed -i "s/'PORT': '3306'/'PORT': '$db_port'/g" "$BITNAMI_PROJECTS_DIR/EvenSight/settings.py"
 
 # Install project dependencies
 echo "Installing project dependencies..."
@@ -33,15 +59,15 @@ $BITNAMI_PYTHON $BITNAMI_MANAGE_PY collectstatic --noinput
 
 # Set proper permissions
 echo "Setting proper permissions..."
-chown -R bitnami:daemon "$BITNAMI_STATIC_PATH"
-chown -R bitnami:daemon "$BITNAMI_MEDIA_PATH"
-find "$BITNAMI_STATIC_PATH" -type f -exec chmod 664 {} \;
-find "$BITNAMI_MEDIA_PATH" -type f -exec chmod 664 {} \;
-find "$BITNAMI_STATIC_PATH" -type d -exec chmod 775 {} \;
-find "$BITNAMI_MEDIA_PATH" -type d -exec chmod 775 {} \;
+chown -R bitnami:daemon "$BITNAMI_STATIC_DIR"
+chown -R bitnami:daemon "$BITNAMI_MEDIA_DIR"
+find "$BITNAMI_STATIC_DIR" -type f -exec chmod 664 {} \;
+find "$BITNAMI_MEDIA_DIR" -type f -exec chmod 664 {} \;
+find "$BITNAMI_STATIC_DIR" -type d -exec chmod 775 {} \;
+find "$BITNAMI_MEDIA_DIR" -type d -exec chmod 775 {} \;
 
 # Restart Apache to apply changes
 echo "Restarting Apache server..."
-/opt/bitnami/ctlscript.sh restart apache
+$BITNAMI_BASE_DIR/ctlscript.sh restart apache
 
 echo "Django deployment script completed."
